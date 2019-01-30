2c71ba9100268d4ef89a74a62692fe1e9ca485ca
Tristan Straub <tristanstraub@gmail.com>
2019-01-31 07:51:49 +1100

    Split up into functions

--- src/busmaker2/client.cljs
+++ src/busmaker2/client.cljs
@@ -27,72 +27,78 @@
      :pixi.shape/fill     {:pixi.fill/color 0xffffff
                            :pixi.fill/alpha 1}}))
 
+(defn pointer-down!
+  [app-state-atom]
+  (fn [^js/PIXI.interaction.InteractionEvent e]
+    (let [app-state      @app-state-atom
+          {:keys [drag]} app-state
+          {:keys [x y]}        drag]
+      (swap! app-state-atom update :drag
+             assoc
+             :mx0       (.. e -data -originalEvent -clientX)
+             :my0       (.. e -data -originalEvent -clientY)
+             :x0        x
+             :y0        y
+             :dragging? true))))
+
+(defn pointer-move!
+  [app-state-atom]
+  (fn [^js/PIXI.interaction.InteractionEvent e]                                                               
+    (let [app-state      @app-state-atom
+          {:keys [drag]} app-state]
+      (when (:dragging? drag)
+                                                                   
+        (let [{:keys [x y
+                      mx0 my0
+                      x0 y0]} drag
+              dmx (- mx0 (.. e -data -originalEvent -clientX))
+              dmy (- my0 (.. e -data -originalEvent -clientY))]
+          (swap! app-state-atom update :drag
+                 assoc
+                 :x (- x0 dmx)
+                 :y (- y0 dmy)))))))
+
+(defn pointer-up!
+  [app-state-atom]
+  (fn [_]
+    (let [app-state      @app-state-atom
+          {:keys [drag]} app-state]
+      (swap! app-state-atom update :drag assoc :dragging? false))))
+
+(defn stage
+  [app-state-atom {:keys [width height drag] :as app-state}]
+  {:pixi/renderer {:pixi.renderer/size [width height]
+                   :pixi.renderer/background-color 0x000000}
+
+   :pixi/listeners {:pointer-down (pointer-down! app-state-atom)
+                    :pointer-move (pointer-move! app-state-atom)
+                    :pointer-up   (pointer-up! app-state-atom)}
+
+   :pixi/stage    {:impi/key :background
+                   :pixi.object/type :pixi.object.type/container
+                   :pixi.container/children
+                   [{:impi/key                      :stage
+                     :pixi.object/type              :pixi.object.type/container
+                     :pixi.object/position          [(get drag :x 0)
+                                                     (get drag :y 0)]
+
+                     :pixi.object/scale             [(get drag :scale-x 1)
+                                                     (get drag :scale-y 1)]
+
+                     :pixi.object/interactive?      true
+                     :pixi.object/contains-point    (constantly true)
+                     :pixi.event/pointer-down       [:pointer-down]
+                     :pixi.event/pointer-up         [:pointer-up]
+                     :pixi.event/pointer-move       [:pointer-move]
+                     :pixi.container/children
+
+                     [{:impi/key             :stars
+                       :pixi.object/type     :pixi.object.type/graphics
+                       :pixi.graphics/shapes stars}]}]}})
+
 (defn render-stage!
   [el stage-id app-state-atom app-state]
-  (let [[w h]                [(:width app-state)
-                              (:height app-state)]
-        {:keys [drag]} app-state
-        stage                {:pixi/renderer {:pixi.renderer/size [w h]
-                                              :pixi.renderer/background-color 0x000000}
-
-                              :pixi/listeners {:pointer-down (fn [^js/PIXI.interaction.InteractionEvent e]
-                                                               (let [app-state      @app-state-atom
-                                                                     {:keys [drag]} app-state
-                                                                     {:keys [x y]}        drag]
-                                                                 (swap! app-state-atom update :drag
-                                                                        assoc
-                                                                        :mx0       (.. e -data -originalEvent -clientX)
-                                                                        :my0       (.. e -data -originalEvent -clientY)
-                                                                        :x0        x
-                                                                        :y0        y
-                                                                        :dragging? true)))
-
-                                               :pointer-move (fn [^js/PIXI.interaction.InteractionEvent e]
-                                                               
-                                                               (let [app-state      @app-state-atom
-                                                                     {:keys [drag]} app-state]
-                                                                 (when (:dragging? drag)
-                                                                   
-                                                                   (let [{:keys [x y
-                                                                                 mx0 my0
-                                                                                 x0 y0]} drag
-                                                                         dmx (- mx0 (.. e -data -originalEvent -clientX))
-                                                                         dmy (- my0 (.. e -data -originalEvent -clientY))]
-                                                                     (swap! app-state-atom update :drag
-                                                                            assoc
-                                                                            :x (- x0 dmx)
-                                                                            :y (- y0 dmy))))))
-
-                                               :pointer-up   (fn [_]
-                                                               (let [app-state      @app-state-atom
-                                                                     {:keys [drag]} app-state]
-                                                                 (swap! app-state-atom update :drag assoc :dragging? false)))}
-
-                              :pixi/stage    {:impi/key :background
-                                              :pixi.object/type :pixi.object.type/container
-                                              :pixi.container/children
-                                              [{:impi/key                      :stage
-                                                :pixi.object/type              :pixi.object.type/container
-                                                :pixi.object/position          [(get drag :x 0)
-                                                                                (get drag :y 0)]
-
-                                                :pixi.object/scale             [(get drag :scale-x 1)
-                                                                                (get drag :scale-y 1)]
-
-                                                :pixi.object/interactive?      true
-                                                :pixi.object/contains-point    (constantly true)
-                                                :pixi.event/pointer-down       [:pointer-down]
-                                                :pixi.event/pointer-up         [:pointer-up]
-                                                :pixi.event/pointer-move       [:pointer-move]
-                                                :pixi.container/children
-
-                                                [{:impi/key             :stars
-                                                  :pixi.object/type     :pixi.object.type/graphics
-                                                  :pixi.graphics/shapes stars}]}]}}]
-
-    (impi/mount stage-id
-                stage
-                el)))
+  (impi/mount stage-id (stage app-state-atom app-state) el))
 
 (def impi
   {:did-mount (fn [state]
@@ -166,6 +172,22 @@
                         (- (old-pos 1) (- (new-screen-pos 1) y))]]
     [new-stage-pos new-scale]))
 
+(defn listen!
+  []
+  (fn [^js/WheelEvent e]
+    (.preventDefault e)
+
+    (let [[new-stage-pos new-scale] (zoom [(get (:drag @state) :x 0)
+                                           (get (:drag @state) :y 0)]
+                                          [(get (:drag @state) :scale-x 1)
+                                           (get (:drag @state) :scale-y 1)]
+                                          (- (.. e -deltaY)) (.. e -offsetX) (.. e -offsetY))]
+      (swap! state update :drag (fn [drag]
+                                  (assoc drag
+                                         :x (new-stage-pos 0)
+                                         :y (new-stage-pos 1)
+                                         :scale-x (new-scale 0)
+                                         :scale-y (new-scale 1)))))))
 
 (defn ^:expose main
   []
@@ -175,26 +197,9 @@
 
     (swap! state assoc :width w :height h)
     
-    (.addEventListener (dom/getElement "app")
-                       "mousewheel"
-                       (fn [^js/WheelEvent e]
-                         (.preventDefault e)
-
-                         (let [[new-stage-pos new-scale] (zoom [(get (:drag @state) :x 0)
-                                                                (get (:drag @state) :y 0)]
-                                                               [(get (:drag @state) :scale-x 1)
-                                                                (get (:drag @state) :scale-y 1)]
-                                                               (- (.. e -deltaY)) (.. e -offsetX) (.. e -offsetY))]
-                           (swap! state update :drag (fn [drag]
-                                                       (assoc drag
-                                                              :x (new-stage-pos 0)
-                                                              :y (new-stage-pos 1)
-                                                              :scale-x (new-scale 0)
-                                                              :scale-y (new-scale 1)))))))
+    (.addEventListener (dom/getElement "app") "mousewheel" (listen!))
 
     (r/mount (root state)
              (dom/getElement "app"))))
 
-;; for reloading onchanges
-(reset! state @state)
 
